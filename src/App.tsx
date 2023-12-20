dc934e60c676adb58833b358191706c47bd0a80f
hellpains <rustam2004sadulaev@mail.ru>
2023-12-20 06:00:24 +0300

    add typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
-
-import { TextField } from '@/components/ui/text-field'
+import { Typography } from '@/components/ui/typography'
 
 export function App() {
-  const [value, setValue] = useState('wefwef')
-
   return (
     <div>
-      <div style={{ width: '900px' }}>
-        <TextField
-          error={'ERROR!!!'}
-          label={'Input'}
-          onValueChange={setValue}
-          placeholder={'Input'}
-          search
-          value={value}
-        />
-      </div>
+      <Typography as={'a'} href={'#'} style={{ color: 'white' }} variant={'large'}>
+        hesdfsdfsdfsdfllo
+      </Typography>
     </div>
   )
 }
---
+++ src/components/ui/typography/typography.tsx
@@ -0,0 +1,28 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import s from './typography.module.scss'
+type TypographyProps<T extends ElementType = 'span'> = {
+  as?: T
+  className?: string
+  variant:
+    | 'body1'
+    | 'body2'
+    | 'caption'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'large'
+    | 'link1'
+    | 'link2'
+    | 'overline'
+    | 'subtitle1'
+    | 'subtitle2'
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType = 'span'>(
+  props: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>
+) => {
+  const { as: Component = 'span', className, variant, ...rest } = props
+
+  return <Component className={`${s[variant]} ${className} ${s.typography}`} {...rest} />
+}
